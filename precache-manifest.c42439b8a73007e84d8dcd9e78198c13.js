892e90dfa1886a8216da75a3bea2119476dcff67
vinc86 <vincenzomancuso86@gmail.com>
2020-09-01 09:49:35 +0200

    Updates

---
+++ precache-manifest.c42439b8a73007e84d8dcd9e78198c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b80977cee4d1aaaa0b2053a6cc8e73c",
+    "url": "/vinc86/todo-app-react/index.html"
+  },
+  {
+    "revision": "b6f7ff2d2225bba7644f",
+    "url": "/vinc86/todo-app-react/static/css/main.e95605cc.chunk.css"
+  },
+  {
+    "revision": "d818c45396790b35d79f",
+    "url": "/vinc86/todo-app-react/static/js/2.f60b7958.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vinc86/todo-app-react/static/js/2.f60b7958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f7ff2d2225bba7644f",
+    "url": "/vinc86/todo-app-react/static/js/main.a46cc219.chunk.js"
+  },
+  {
+    "revision": "ebb5a770cf916d042c7c",
+    "url": "/vinc86/todo-app-react/static/js/runtime-main.8f58781a.js"
+  }
+]);
